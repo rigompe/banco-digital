0e309cb3c15a1b466da5358bd7bfcb9bc87065dd
rigompe <ricardo.gomesp@outlook.com>
2021-08-02 22:47:14 -0300

    updated to make person and employee abstract class

--- src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -1,18 +1,7 @@
 package one.digitalinnovation.digionebank
 
-class Pessoa {
-    var nome: String = "ricardo"
-    var cpf: String = "229.183.117-11"
-    private set
+abstract class Pessoa(
+    val nome: String,
+    val cpf: String
+)
 
-    constructor()
-
-    fun pessoainfo() = "$nome e $cpf"
-}
-
-fun main() {
-    val ricardo = Pessoa()
-
-    println(ricardo.pessoainfo())
-
-}
---
+++ src/main/kotlin/one/digitalinnovation/digionebank/testes/TesteFuncionario.kt
@@ -0,0 +1,16 @@
+package one.digitalinnovation.digionebank.testes
+
+import one.digitalinnovation.digionebank.Funcionario
+import one.digitalinnovation.digionebank.Pessoa
+import java.math.BigDecimal
+
+fun main() {
+//    val ricardo = Pessoa(nome = "Ricardo Gomes", cpf = "229.658.236-86")
+//    println(ricardo.nome)
+//    println(ricardo.cpf)
+//
+//    val jose = Funcionario(nome = "Jose Silva", cpf = "263.547.568-56", BigDecimal.valueOf(2000.0))
+//    println(jose.nome)
+//    println(jose.cpf)
+//    println(jose.salario)
+}
